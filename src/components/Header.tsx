0e9b5b22a630eb2879da97b22994fe61d69b4263
hyojoo <jhj080802@naver.com>
2024-09-04 18:18:42 +0900

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,43 +5,51 @@ import { Link, useLocation } from "react-router-dom";
 const Header: React.FC = () => {
   const location = useLocation();
   const currentPath = location.pathname;
-
   return (
-    <header className="bg-blue-500 text-white p-6 flex flex-col sm:flex-row items-center">
-      <h1 className="text-xl font-bold flex-grow sm:mr-4 text-left">
-        Hyo joo's Portfolio
-      </h1>
-      <nav className="mt-4 sm:mt-0 flex-grow flex justify-center text-lg">
+    <aside className="w-64 bg-[#882653] text-white h-screen p-4 fixed top-0 left-0 flex flex-col">
+      <h1 className="text-3xl font-bold mb-3 text-white">Menu</h1>
+      <hr />
+      <nav className="flex flex-col space-y-4 mt-3 ">
         <Link
-          className={`mx-2 ${
-            currentPath === "/" ? "text-yellow-200 font-bold" : "text-white"
+          className={`text-lg hover:text-pink-200  ${
+            currentPath === "/" ? "text-yellow-300 font-bold" : "text-white"
           }`}
           to="/"
         >
-          Home
+          Main
         </Link>
         <Link
-          className={`mx-2 ${
+          className={`text-lg hover:text-pink-200  ${
+            currentPath === "/aboutMe"
+              ? "text-yellow-300 font-bold"
+              : "text-white"
+          }`}
+          to="/aboutMe"
+        >
+          About Me
+        </Link>
+        <Link
+          className={`text-lg hover:text-pink-300  ${
             currentPath === "/projects"
-              ? "text-yellow-200 font-bold"
+              ? "text-yellow-300 font-bold"
               : "text-white"
           }`}
           to="/projects"
         >
           Projects
         </Link>
-        <Link
-          className={`mx-2 ${
+        {/* <Link
+          className={`text-lg hover:text-pink-300  ${
             currentPath === "/contact"
-              ? "text-yellow-200 font-bold"
+              ? "text-yellow-300 font-bold"
               : "text-white"
           }`}
           to="/contact"
         >
           Contact
-        </Link>
+        </Link> */}
       </nav>
-    </header>
+    </aside>
   );
 };
 
