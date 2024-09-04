f8115e08c515d837545e29c56a5c80e9ccafee03
hyojoo <jhj080802@naver.com>
2024-09-04 18:15:57 +0900

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,22 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}", // 파일 경로 설정
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "roboto-slab": ['"Roboto Slab"', "serif"],
+        hahmlet: ["Hahmlet", "serif"],
+      },
+      fontWeight: {
+        "extra-light": "100",
+        light: "200",
+        normal: "400",
+        medium: "500",
+        "semi-bold": "600",
+        bold: "700",
+        "extra-bold": "800",
+        black: "900",
+      },
+    },
   },
   plugins: [],
 };
