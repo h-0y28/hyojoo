a316734764b23c103df29062e2ab783b0359736f
hyojoo <jhj080802@naver.com>
2024-09-04 18:19:42 +0900

    change header location and rename home page -> main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,25 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import AboutMe from "./pages/aboutMe";
+import Main from "./pages/Main";
 
 const App: React.FC = () => {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="flex">
+        <Header />
+        <div className="ml-64 flex-1">
+          <Routes>
+            <Route path="/aboutMe" element={<AboutMe />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
     </Router>
   );
 };
